ec94bf27d0b303fabf4a41fa05131045103af833
Chad McElligott <hotsw4p@gmail.com>
2013-02-23 10:32:48 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+this is a test of the emergency broadcast system.
+
+THIS IS ONLY A TEST
+===================
---
+++ test/test.js
@@ -0,0 +1,10 @@
+var should = require('should');
+var assert = require('assert');
+
+describe('the initial test', function(){
+  it('should just be a stub waiting for some functionality', function(done){
+    should.exist(should);
+    should.exist(assert);
+    done();
+  });
+});
