69a48a4238b38be100ebad8e85bffa6ad7e5febc
Chad McElligott <hotsw4p@gmail.com>
2013-02-25 22:31:31 -0600

    added support for saving multiple articles, added more tests

---
+++ test/articleprovider-tests.js
@@ -0,0 +1,147 @@
+var should = require("should");
+var assert = require("assert");
+
+describe("article provider", function () {
+
+  var articleProvider = require("../lib/articleprovider");
+  var testArticleID;
+
+  beforeEach(function (done) {
+
+    var testArticle = {
+      title: "test article",
+      body: "test body",
+      author: "test@email.com"
+    };
+
+    articleProvider.clear(function (err) {
+          debugger;
+      articleProvider.save(testArticle, function (err, savedArticles) {
+        testArticleID = savedArticles[0].id;
+        done();
+      });
+    });
+  });
+
+  describe("#findAll()", function () {
+
+    it("should retrieve all articles from storage", function (done) {
+      var someArticles = [
+        {title: "t1", body: "b1", author: "me@here.com"},
+        {title: "t2", body: "b2", author: "me@here.com"},
+        {title: "t3", body: "b3", author: "me@here.com"}
+      ];
+
+      articleProvider.save(someArticles, function (err, savedArticles) {
+        articleProvider.findAll(function (err, allArticles) {
+          should.not.exist(err);
+          allArticles.length.should.equal(4);
+          done();
+        });
+      });
+    });
+  });
+
+  describe("#clear()", function () {
+
+    it("should remove all articles from storage", function (done) {
+      articleProvider.clear(function (err) {
+        should.not.exist(err);
+
+        articleProvider.findAll(function (err, allArticles) {
+          allArticles.length.should.equal(0);
+          done();
+        });
+      });
+    });
+  });
+
+  describe("#findByID()", function () {
+
+    it("should return an article when a valid id is supplied", function (done) {
+      articleProvider.findByID(testArticleID, function (err, foundArticle) {
+        should.not.exist(err);
+        foundArticle.id.should.equal(testArticleID);
+        done();
+      });
+    });
+
+    it("should return an error when an invalid id is supplied", function (done) {
+      articleProvider.findByID(-1, function (err, foundArticle) {
+        should.exist(err);
+        should.not.exist(foundArticle);
+        done();
+      });
+    });
+  });
+
+  describe("#save()", function () {
+
+    it("should ADD an article when article.id not specified", function (done) {
+      var articleTitle = "Yogi Bear's Picnic";
+
+      var anArticle = {
+        title: articleTitle,
+        body: "The picnic was excellent",
+        author: "yogi@bears.com"
+      };
+
+      articleProvider.save(anArticle, function (err, savedArticles) {
+        should.not.exist(err);
+        savedArticles[0].title.should.equal(articleTitle);
+        savedArticles[0].should.haveOwnProperty("id");
+        savedArticles[0].should.haveOwnProperty("createdAt");
+
+        articleProvider.findAll(function (err, allArticles) {
+          allArticles.length.should.equal(2);
+          done();
+        });
+      });
+    });
+
+    it("should ADD an article when article.id does not match an article existing in storage", function (done) {
+      var anArticle = {
+        id: 35,
+        title: "Specifics are grand",
+        body: "i am specifying the id",
+        author: "cptobvious@gmail.com"
+      };
+
+      articleProvider.save(anArticle, function (err, savedArticles) {
+        should.not.exist(err);
+        should.exist(savedArticles);
+
+        articleProvider.findAll(function (err, allArticles) {
+          allArticles.length.should.equal(2);
+          done();
+        });
+      });
+    });
+
+    it("should EDIT an article when article.id matches an article existing in storage", function (done) {
+
+      articleProvider.findByID(testArticleID, function (err, foundArticle) {
+
+        should.not.exist(err);
+        should.exist(foundArticle);
+        foundArticle.body = "edited body";
+
+        articleProvider.save(foundArticle, function (err, savedArticles) {
+          should.not.exist(err);
+          savedArticles[0].body.should.equal(foundArticle.body);
+          savedArticles[0].id.should.equal(foundArticle.id);
+          savedArticles[0].should.haveOwnProperty("lastUpdatedAt");
+
+          articleProvider.findAll(function (err, allArticles) {
+            allArticles.should.have.length(1);
+            done();
+          });
+        });
+      });
+    });
+
+  });
+
+
+  it("should be able to be delete an article");
+});
