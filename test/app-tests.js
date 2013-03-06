b5cfebd63b77eba35a65c26eff486f20fdebc927
Chad McElligott <hotsw4p@gmail.com>
2013-03-05 19:24:31 -0600

    making room for additional http verbs

--- test/app-tests.js
+++ test/app-tests.js
@@ -9,13 +9,16 @@ describe("routes", function () {
   describe("home routes", function () {
 
     describe("/", function () {
+      describe("GET", function () {
 
-      it("should respond ok", function (done) {
-        req(app)
-        .get("/")
-        .expect(200, done);
+        it("should respond ok", function (done) {
+          req(app)
+          .get("/")
+          .expect("Content-Type", "text/plain")
+          .expect(200, done);
+        });
+
       });
-
     });
   });
 
@@ -46,62 +49,65 @@ describe("routes", function () {
     });
 
     describe("/articles", function() {
+      describe("GET", function () {
 
-      it("should respond ok", function (done) {
-        req(app)
-        .get("/articles")
-        .expect(200, done);
-      });
+        it("should respond ok", function (done) {
+          req(app)
+          .get("/articles")
+          .expect(200, done);
+        });
 
-      it("should list all articles as JSON", function (done) {
-        req(app)
-        .get("/articles")
-        .expect("Content-Type", /json/)
-        .end(function (err, res) {
-          should.not.exist(err);
-          articleProvider.findAll(function (findError, allArticles) {
-            var articlesJSON = JSON.stringify(allArticles, app.get("json replacer"), app.get("json spaces"));
-            articlesJSON.should.equal(res.text);
-            done();
+        it("should list all articles as JSON", function (done) {
+          req(app)
+          .get("/articles")
+          .expect("Content-Type", /json/)
+          .end(function (err, res) {
+            should.not.exist(err);
+            articleProvider.findAll(function (findError, allArticles) {
+              var articlesJSON = JSON.stringify(allArticles, app.get("json replacer"), app.get("json spaces"));
+              articlesJSON.should.equal(res.text);
+              done();
+            });
           });
         });
+
       });
 
     });
 
     describe("/articles/:id", function() {
-
-      it("should retrieve the specified article as JSON", function (done) {
-        req(app)
-        .get("/articles/" + testArticleId)
-        .expect("Content-Type", /json/)
-        .end(function (err, res) {
-          should.not.exist(err);
-
-          articleProvider.findById(testArticleId, function (findError, article) {
-            var articleJSON = JSON.stringify(article, app.get("json replacer"), app.get("json spaces"));
-            articleJSON.should.equal(res.text);
-            done();
-          });
-        });
-      });
-
-      it("should return 404 when an invalid article id is specified", function (done) {
-        req(app)
-        .get("/articles/foo")
-        .expect(404)
-        .end(function (err, resp) {
+      describe("GET", function () {
+        it("should retrieve the specified article as JSON", function (done) {
           req(app)
-          .get("/articles/foo/bar")
+          .get("/articles/" + testArticleId)
+          .expect("Content-Type", /json/)
+          .end(function (err, res) {
+            should.not.exist(err);
+
+            articleProvider.findById(testArticleId, function (findError, article) {
+              var articleJSON = JSON.stringify(article, app.get("json replacer"), app.get("json spaces"));
+              articleJSON.should.equal(res.text);
+              done();
+            });
+          });
+        });
+
+        it("should return 404 when an invalid article id is specified", function (done) {
+          req(app)
+          .get("/articles/foo")
           .expect(404)
-          .end(function (err2, resp2) {
+          .end(function (err, resp) {
             req(app)
-            .get("/articles/" + testArticleId + "/foo")
-            .expect(404, done);
+            .get("/articles/foo/bar")
+            .expect(404)
+            .end(function (err2, resp2) {
+              req(app)
+              .get("/articles/" + testArticleId + "/foo")
+              .expect(404, done);
+            });
           });
         });
       });
-
     });
 
   });
